9a9fbe11f321839901a65c24573c938dd3bdff8e
shengming <975601874@qq.com>
2023-09-07 10:44:31 +0800

    feat:dockerfile

--- ruoyi-framework/src/main/java/com/ruoyi/framework/web/service/SysLoginService.java
+++ ruoyi-framework/src/main/java/com/ruoyi/framework/web/service/SysLoginService.java
@@ -58,7 +58,7 @@ public class SysLoginService
      * @param username 用户名
      * @param password 密码
      * @param code 验证码
-     * @param uuid 唯一标识
+     * @param uuid 验证码唯一标识
      * @return 结果
      */
     public String login(String username, String password, String code, String uuid)
